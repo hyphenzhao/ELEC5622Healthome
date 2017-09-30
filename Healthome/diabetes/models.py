9ddc2c87e339ededd7e5e0f635d82cd25c91e481
Haifeng Zhao <haifengzhao@Haifengs-MacBook-Pro.local>
2017-09-30 23:31:07 +1000

    Create models for users login and arduino board association.

--- Healthome/diabetes/models.py
+++ Healthome/diabetes/models.py
@@ -2,7 +2,13 @@
 from __future__ import unicode_literals
 
 from django.db import models
+from django.contrib.auth.models import User
 
-
-
+class ArduinoBoard(models.Model):
+	user = models.ForeignKey(
+		User,
+		on_delete=models.CASCADE,
+	)
+	board = models.CharField(max_length=30)
+	used = models.BooleanField(default=False)
 # Create your models here.
