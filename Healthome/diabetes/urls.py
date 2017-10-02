64293cc50f5b77723085de31722f23e68f66f0cb
fittting <Chenmachinist@gmail.com>
2017-10-02 04:20:41 -0700

    Implement input function. Modified the urls.py for others to use.

--- Healthome/diabetes/urls.py
+++ Healthome/diabetes/urls.py
@@ -5,4 +5,7 @@ from . import views
 urlpatterns = [
     url(r'^$', views.index, name='index'),
     url(r'^profile/$',views.profile, name='profile'),
+    url(r'^input/$',views.input, name='input'),
+    url(r'^arduino/$',views.arduino, name='arduino'),
+    url(r'^result/$',views.result, name='result'),
 ]
