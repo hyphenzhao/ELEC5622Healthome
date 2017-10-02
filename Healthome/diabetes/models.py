0e3fa71fbd523ae490c01cd4e0c70ebc94cd270a
Haifeng Zhao <hzha7521@uni.sydney.edu.au>
2017-10-02 22:15:33 +1100

    Update models.py to make it supports input and arduino function.

--- Healthome/diabetes/models.py
+++ Healthome/diabetes/models.py
@@ -9,6 +9,29 @@ class ArduinoBoard(models.Model):
 		User,
 		on_delete=models.CASCADE,
 	)
-	board = models.CharField(max_length=30)
+	board = models.CharField(max_length=30, unique=True)
 	used = models.BooleanField(default=False)
+
+class InputBodyStatus(models.Model):
+	user = models.ForeignKey(
+		User,
+		on_delete=models.CASCADE,
+	)
+	pregnant = models.DecimalField(max_digits=25, decimal_places=15)
+	skinfold = models.DecimalField(max_digits=25, decimal_places=15)
+	seruminsulin = models.DecimalField(max_digits=25, decimal_places=15)
+	bmi = models.DecimalField(max_digits=25, decimal_places=15)
+	pedigree = models.DecimalField(max_digits=25, decimal_places=15)
+	age = models.DecimalField(max_digits=25, decimal_places=15)
+	inputdate = models.DateTimeField(auto_now_add=True, blank=True)
+
+class TestBodyStatus(models.Model):
+	user = models.ForeignKey(
+		User,
+		on_delete=models.CASCADE,
+	)
+	glucose = models.DecimalField(max_digits=25, decimal_places=15)
+	bloodpresure = models.DecimalField(max_digits=25, decimal_places=15)
+	testdate = models.DateTimeField(auto_now_add=True, blank=True)
+
 # Create your models here.
