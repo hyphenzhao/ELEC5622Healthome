1c7907abe9900efdb8e89150ff7d789b37b6903e
wshi3699 <wshi3699@uni.sydney.edu.au>
2017-10-28 01:50:05 -0700

    Create input test. resolve #36

--- Healthome/diabetes/tests.py
+++ Healthome/diabetes/tests.py
@@ -9,6 +9,64 @@ from django.urls import reverse
 from .models import *
 from . import views
 
+class DiabetesInputTest(TestCase):
+	def setUp(self):
+		user = User.objects.create_user(
+				username = "test@test.com",
+				password = "123456",
+				email = "test@test.com",
+				first_name = "firstname",
+				last_name = "lastname"
+			)
+		user.save()
+		board = ArduinoBoard(
+					user=user,
+					board="123456",
+					used='True'
+				)
+		board.save()
+		inputstatus = InputBodyStatus(
+				user=user,
+				pregnant="1",
+				skinfold="20",
+				seruminsulin="20",
+				bmi="20",
+				pedigree="0.5",
+				age="20"
+			)
+		inputstatus.save()
+
+	def test_input_view(self):
+		data = {
+			"email": "test@test.com",
+			"password": "123456"
+		}
+		self.client.post('/diabetes/', data=data, follow=True)
+		response = self.client.get('/diabetes/input/', follow=True)
+		self.assertEqual(response.status_code, 200)
+		self.assertEqual(response.resolver_match.func, views.input)
+		self.assertContains(response, "20")
+		self.assertContains(response, "0.5")
+		self.assertContains(response, "input")
+
+	def test_input_new(self):
+		data = {
+			"email": "test@test.com",
+			"password": "123456"
+		}
+		self.client.post('/diabetes/', data=data, follow=True)
+		input_data = {
+			"pregnant": "1",
+			"skinfold": "201",
+			"seruminsulin": "201",
+			"bmi": "201",
+			"pedigree": "0.52",
+			"age": "201"
+		}
+		response = self.client.post('/diabetes/input/', data=input_data, follow=True)
+		self.assertContains(response, "201")
+		self.assertContains(response, "Body status saved successfully!")
+
 class DiabetesProfileTest(TestCase):
 	def setUp(self):
 		user = User.objects.create_user(
