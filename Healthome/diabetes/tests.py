d91cce2ee36083d49558558dd45ab0631f63cbe8
Haifeng Zhao <hzha7521@uni.sydney.edu.au>
2017-10-28 19:20:07 +1100

    Add test. resolve #38 resolve #39

--- Healthome/diabetes/tests.py
+++ Healthome/diabetes/tests.py
@@ -2,13 +2,95 @@
 from __future__ import unicode_literals
 
 from django.test import TestCase
+from django.test.client import Client
 
 from django.urls import reverse
 
-class DiabetesIndexViewTest(TestCase):
-	def test_hello_world(self):
-		response = self.client.get(reverse('index'))
+from .models import *
+from . import views
+
+class DiabetesResultTest(TestCase):
+	def setUp(self):
+		user = User.objects.create_user(
+				username = "test@test.com",
+				password = "123456",
+				email = "test@test.com",
+				first_name = "firstname",
+				last_name = "lastname"
+			)
+		user.save()
+		board = ArduinoBoard(
+					user=user,
+					board="123456",
+					used='True'
+				)
+		board.save()
+		arduino_status = TestBodyStatus(
+				user=user,
+				glucose="20",
+				bloodpressure="70"
+			)
+		arduino_status.save()
+		inputstatus = InputBodyStatus(
+				user=user,
+				pregnant="1",
+				skinfold="20",
+				seruminsulin="20",
+				bmi="20",
+				pedigree="0.5",
+				age="20"
+			)
+		inputstatus.save()
+
+	def test_result_view(self):
+		data = {
+			"email": "test@test.com",
+			"password": "123456"
+		}
+		self.client.post('/diabetes/', data=data, follow=True)
+		response = self.client.get('/diabetes/result/', follow=True)
 		self.assertEqual(response.status_code, 200)
-		self.assertContains(response, "Hello, world. You're at the diabetes application index.")
+		self.assertNotContains(response, "%")
+
+	def test_result_add(self):
+		data = {
+			"email": "test@test.com",
+			"password": "123456"
+		}
+		self.client.post('/diabetes/', data=data, follow=True)
+		result_data = {
+			"add": "add"
+		}
+		response = self.client.post('/diabetes/result/', data=result_data, follow=True)
+		self.assertEqual(response.status_code, 200)
+		self.assertContains(response, "%")
+
+	def test_result_delete(self):
+		data = {
+			"email": "test@test.com",
+			"password": "123456"
+		}
+		self.client.post('/diabetes/', data=data, follow=True)
+		result_data = {
+			"add": "add"
+		}
+		response = self.client.post('/diabetes/result/', data=result_data, follow=True)
+		self.assertEqual(response.status_code, 200)
+		self.assertContains(response, "%")
+		result_data = {
+			"delete": "delete",
+			"recordID": 1
+		}
+		response = self.client.post('/diabetes/result/', data=result_data, follow=True)
+		self.assertEqual(response.status_code, 200)
+		self.assertNotContains(response, "%")
+
+
+class DiabetesTestTest(TestCase):
+
+	def test_test_view(self):
+		response = self.client.get('/diabetes/test/', follow=True)
+		self.assertEqual(response.status_code, 200)
+		self.assertContains(response, "Nothing posted.")
 
 # Create your tests here.
