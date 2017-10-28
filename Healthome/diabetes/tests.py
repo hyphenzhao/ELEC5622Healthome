c39875f854cc72b867b6d3c78b237209a8ffc197
ivan <yzha8811@uni.sydney.edu.au>
2017-10-28 19:45:35 +1100

    Create profile test. resolve #37

--- Healthome/diabetes/tests.py
+++ Healthome/diabetes/tests.py
@@ -9,6 +9,64 @@ from django.urls import reverse
 from .models import *
 from . import views
 
+class DiabetesProfileTest(TestCase):
+	def setUp(self):
+		user = User.objects.create_user(
+				username = "test@test.com",
+				password = "123456",
+				email = "test@test.com",
+				first_name = "firstname",
+				last_name = "lastname"
+			)
+		user.save()
+		board = ArduinoBoard(
+					user=user,
+					board="123456",
+					used='True'
+				)
+		board.save()
+
+	def test_profile_view(self):
+		data = {
+			"email": "test@test.com",
+			"password": "123456"
+		}
+		self.client.post('/diabetes/', data=data, follow=True)
+		response = self.client.get('/diabetes/profile/', follow=True)
+		self.assertEqual(response.status_code, 200)
+		self.assertEqual(response.resolver_match.func, views.profile)
+		self.assertContains(response, "firstname")
+		self.assertContains(response, "lastname")
+
+	def test_profile_add_new_board(self):
+		data = {
+			"email": "test@test.com",
+			"password": "123456"
+		}
+		self.client.post('/diabetes/', data=data, follow=True)
+		board_data = {
+			"add": "add",
+			"add_board": "1234567890"
+		}
+		response = self.client.post('/diabetes/profile/', data=board_data, follow=True)
+		self.assertEqual(response.status_code, 200)
+		self.assertEqual(response.resolver_match.func, views.profile)
+		self.assertContains(response, "1234567890")
+
+	def test_profile_delete_board(self):
+		data = {
+			"email": "test@test.com",
+			"password": "123456"
+		}
+		self.client.post('/diabetes/', data=data, follow=True)
+		board_data = {
+			"delete": "delete",
+			"delete_board": "123456"
+		}
+		response = self.client.post('/diabetes/profile/', data=board_data, follow=True)
+		self.assertRedirects(response,'/diabetes/profile/')
+		self.assertNotContains(response, "123456")
+
 class DiabetesResultTest(TestCase):
 	def setUp(self):
 		user = User.objects.create_user(
